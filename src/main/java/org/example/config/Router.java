119a2cdded83c9c38459a5b1c249329ff4cdce9a
vasiliyChobot <i@vchobot.ru>
2022-12-25 18:11:32 +0300

    initial commit

---
+++ src/main/java/org/example/config/Router.java
@@ -0,0 +1,15 @@
+package org.example.config;
+
+import io.javalin.Javalin;
+
+import static io.javalin.apibuilder.ApiBuilder.get;
+import static io.javalin.apibuilder.ApiBuilder.post;
+
+/**
+ * @author andreiserov
+ */
+public class Router {
+
+    public static void register(Javalin app) {
+    }
+}
